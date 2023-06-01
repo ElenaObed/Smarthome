1dbe5cb04254f4ecdb05be3d709b3b181fbe6e10
Elena Obed <elena_sergeevna137@mail.ru>
2023-06-01 18:35:28 +0300

    initial commit1

---
+++ src/main/java/Radio.java
@@ -0,0 +1,54 @@
+public class Radio {
+    public int сurrentStationNumber;
+    public int currentVolume;
+
+    public int getСurrentStationNumber() {
+        return сurrentStationNumber;
+    }
+
+    public void setСurrentStationNumber(int newCurrentStationNumber) {
+        if (newCurrentStationNumber < 0) {
+            return;
+        } else if (newCurrentStationNumber > 9) {
+            return;
+        }
+        сurrentStationNumber = newCurrentStationNumber;
+    }
+
+    public void nextRadioStation() {
+        if (сurrentStationNumber < 9) {
+            сurrentStationNumber = сurrentStationNumber + 1;
+        } else
+            сurrentStationNumber = 0;
+    }
+
+    public void prevRadioStation() {
+        if (сurrentStationNumber < 0) {
+            сurrentStationNumber = 9;
+        } else
+            сurrentStationNumber = сurrentStationNumber - 1;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+
+        public void increaseVolume () {
+            if (currentVolume < 100) {
+                currentVolume = currentVolume + 1;
+            } else
+                currentVolume = 100;
+
+
+        }
+        public void decreaseVolume () {
+            if (currentVolume > 0) {
+                currentVolume = currentVolume - 1;
+            } else
+                currentVolume = 0;
+
+
+        }
+
+    }
+}
+
