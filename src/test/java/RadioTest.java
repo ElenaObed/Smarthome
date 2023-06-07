6d1980ccce41f2496892c209768553ccc5dbe9c8
Elena Obed <elena_sergeevna137@mail.ru>
2023-06-07 15:03:28 +0300

    newzadan

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -115,7 +115,7 @@ class RadioTest {
         int volume = 50;
         radio.setCurrentVolume(volume);
         radio.increaseVolume();
-        int expected = 51;gir
+        int expected = 51;
 
         Assertions.assertEquals(expected, radio.getCurrentVolume());
     }
